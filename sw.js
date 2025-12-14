492ec821e4bddbaf49232e4563d91f33ffb6301e
studiodiametrodev <studiodiametro.dev@gmail.com>
2025-12-15 00:39:33 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', (e) => {
     e.waitUntil(
         caches.open('parcheggio-v1').then((cache) => {
-            return cache.addAll(['./index.html', './sw.js']);
+            return cache.addAll(['./index.html', './sw.js', './img/logo-192x192.svg', './img/logo-512x512.svg']);
         })
     );
 });
